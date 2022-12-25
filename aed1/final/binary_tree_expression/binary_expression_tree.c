eb47581594f81c655c642b758b79e9cf84c7cf28
Robertt-S <77414106+Robertt-S@users.noreply.github.com>
2022-12-24 21:09:37 -0300

    AED_ALMOST_FINAL

---
+++ aed1/final/binary_tree_expression/binary_expression_tree.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct treeNode {
+  char info;
+  struct treeNode *leftChildTree;
+  struct treeNode *rightChildTree;
+};
+
+typedef struct treeNode typeTreeNode;
+
+typedef typeTreeNode *typeTree;
+
+
+
+int main() {
+  infixExpression[] = {'(', 'A', '+', 'B', ')', , , , , , }
+  
+  
+  return 0;
+}
