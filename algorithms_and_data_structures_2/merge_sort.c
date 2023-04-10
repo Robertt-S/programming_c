992d7cbdad2f41fc089987999fb1607bcc7d320e
Robertt-S <77414106+Robertt-S@users.noreply.github.com>
2023-04-09 23:26:26 -0300

    merge_sort_linked_list

--- algorithms_and_data_structures_2/merge_sort.c
+++ algorithms_and_data_structures_2/merge_sort.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
+typedef struct node {
+  int data;
+  struct node *next;
+} TypeNode;
+
+
+typedef struct list {
+  TypeNode *listInitial;
+  int last;
+} TypeList;
 
 int mergeSort(int*, int, int);
 int merge(int*, int, int, int);
 
 
+int listIsEmpty(TypeList*);
+int insertAtBeginning(TypeList*, int);
+int insertAtEnd(TypeList*, int);
+int insertByChoose(TypeList*, int, int);
 
 
 int main() {
@@ -20,6 +33,11 @@ int main() {
     array[i] = arrayLength - i;
   }
   
+  for (i = 0; i < arrayLength; i++) {
+    printf("%d ", array[i]);
+  }
+  
+  printf("\n");
   
   mergeSort(array, 0, arrayLength - 1);
   
@@ -39,12 +57,9 @@ int main() {
 
 
 int mergeSort(int *array, int arrayInit, int arrayEnd) {
-  int arrayMid;
   
   if (arrayInit < arrayEnd) {
-    arrayMid = (arrayInit + arrayEnd) / 2;
-    
-    printf("arrayMid = %d\n", arrayMid);
+    int arrayMid = (arrayInit + arrayEnd) / 2;
     
     mergeSort(array, arrayInit, arrayMid);
     mergeSort(array, arrayMid + 1, arrayEnd);
@@ -57,53 +72,220 @@ int mergeSort(int *array, int arrayInit, int arrayEnd) {
 
 
 int merge(int *array, int arrayInit, int arrayMid, int arrayEnd) {
-  int i, arrayLength1 = (arrayMid - arrayInit) + 1,
-      arrayLength2 = arrayEnd - arrayMid,
+  int leftArrayLength = arrayMid - arrayInit + 1,
+      rightArrayLength = arrayEnd - arrayMid,
       
-      *array1, *array2;
-      
-  array1 = malloc((arrayLength1 - 1) * sizeof(int));
-  array2 = malloc((arrayLength2 - 1) * sizeof(int));
+      *leftArray, *rightArray,
+      i, j, k;
+   
   
-  for (i = 0; i < arrayLength1; i++) {
-    array1[i] = array[arrayInit + i];
+  leftArray = malloc((leftArrayLength + 1) * sizeof(int));
+  rightArray = malloc((rightArrayLength + 1) * sizeof(int));
+  
+  for (i = 0; i < leftArrayLength; i++) {
+    leftArray[i] = array[arrayInit + i];
   }
   
-  for (i = 0; i < arrayLength2; i++) {
-    array2[i] = array[arrayMid + i + 1];
+  for (j = 0; j < rightArrayLength; j++) {
+    rightArray[j] = array[arrayMid + j + 1];
   }
+  
+  
+  leftArray[leftArrayLength] = 1215752192;
+  rightArray[rightArrayLength] = 1215752192;
+  
+  i = 0;
+  j = 0;
+  
+  for (k = arrayInit; k < arrayEnd + 1; k++) {
+    if (leftArray[i] <= rightArray[j]) {
+      array[k] = leftArray[i];
+      i++;
+    } else {
+      array[k] = rightArray[j];
+      j++;
+    }
+  }
+  
+  free(leftArray);
+  free(rightArray);
+  
+  return 0;
+}
+
+
+int listIsEmpty(TypeList *list) {
+  return list->last == 0;
+}
+
+
+int insertAtBeginning(TypeList *list, int item) {
+  TypeNode *newNode;
+  
+  newNode = (TypeNode*) malloc(sizeof(TypeNode));
+  
+  if (newNode != NULL) {
+    newNode->data = item;
+    newNode->next = list->listInitial;
+    list->listInitial = newNode;
+    list->last++;
+    
+    return 1;
+  }
+  
+  return 0;
+}
+
+
+int insertAtEnd(TypeList *list, int item) {
+  TypeNode *newNode, *pAux;
+  
+  if (listIsEmpty(list)) {
+    insertAtBeginning(list, item);
+    
+    return 1;
+  }
+  
+  newNode = (TypeNode*) malloc(sizeof(TypeNode));
+  
+  if (newNode != NULL) {
+    pAux = list->listInitial;
+    
+    while (pAux->next != NULL) {
+      pAux = pAux->next;
+    }
+    
+    newNode->data = item;
+    newNode->next = NULL;
+    pAux->next = newNode;
+    
+    list->last++;
+    
+    return 1;
+  }
+  
+  return 0;
+}
+
+
+int insertByChoose(TypeList *list, int item, int position) {
+  TypeNode *newNode, *pAux;
+  int i;
+  
+  if (listIsEmpty(list)) {
+    insertAtBeginning(list, item);
+    
+    return 1;
+  } else if (position == list->last + 1) {
+    insertAtEnd(list, item);
+    
+    return 1;
+  } else {
+    
+    newNode = (TypeNode*) malloc(sizeof(TypeNode));
+    
+    pAux = list->listInitial;
+    
+    if (newNode != NULL) {
+      for (i = 2; i < position; i++) {
+        if (pAux->next != NULL) {
+          pAux = pAux->next;
+        }
+      }
+    }
+    
+    newNode->next = pAux->next;
+    pAux->next = newNode;
+    
+    list->last++;
+    
+    return 1;
+  }
+  
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+int merge(int *array, int arrayInit, int arrayMid, int arrayEnd) {
+  int i, leftArrayLength = (arrayMid - arrayInit) + 1,
+      rightArrayLength = arrayEnd - arrayMid,
+      
+      *leftArray, *rightArray;
+      
+  leftArray = malloc((leftArrayLength - 1) * sizeof(int));
+  rightArray = malloc((rightArrayLength - 1) * sizeof(int));
+  
+  for (i = 0; i < leftArrayLength; i++) {
+    leftArray[i] = array[arrayInit + i];
+  }
+  
+  for (i = 0; i < rightArrayLength; i++) {
+    rightArray[i] = array[arrayMid + i + 1];
+  }
+  
+  printf("1: ");
+  for (i = 0; i < leftArrayLength; i++) {
+    printf("%d ", leftArray[i]);
+  }
+  
+  printf("\n");
+  
+  printf("2: ");
+  for (i = 0; i < rightArrayLength; i++) {
+    printf("%d ", rightArray[i]);
+  }
+  
+  printf("\n");
+  
   
   i = 0;
   int j = 0;
   int k = arrayInit;
   
   
-  while (i < arrayLength1 && j < arrayLength2) {
-    if (array1[i] <= array2[j]) {
-      array[k] = array1[i];
+  while (i < leftArrayLength && j < rightArrayLength) {
+    if (leftArray[i] <= rightArray[j]) {
+      array[k] = leftArray[i];
       i++;
     } else {
-      array[k] = array2[j];
+      array[k] = rightArray[j];
       j++;
     }
     k++;
   }
   
-  while (i < arrayLength1) {
-    array[k] = array1[i];
+  while (i < leftArrayLength) {
+    array[k] = leftArray[i];
     i++;
     k++;
   }
   
-  while (j < arrayLength2) {
-    array[k] = array2[j];
+  while (j < rightArrayLength) {
+    array[k] = rightArray[j];
     j++;
     k++;
   }
   
   
-  free(array1);
-  free(array2);
+  free(leftArray);
+  free(rightArray);
   
   return 0;
-}
+}  */
