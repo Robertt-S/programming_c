7954b66de5aaeadd687e361b4a4e4486b47c2b7d
Robertt-S <77414106+Robertt-S@users.noreply.github.com>
2023-05-04 15:32:01 -0300

    radix_counting_removing_redundancy

--- algorithms_and_data_structures_2/counting_radix_sorts.c
+++ algorithms_and_data_structures_2/counting_radix_sorts.c
@@ -18,7 +18,7 @@ int* makePositionsArray(char**, char*, int*, int , int);
 
 
 
-//--- Radix and counting sort implementations
+//--- Radix and counting sort implementations for chars.
 /*
   The first input is the words' number, then the alphabetical key in a chosen order
   and all the words separated by a space.
@@ -55,7 +55,6 @@ int main() {
     scanf("%s", arrayWords1[i]);
   }
   
-  
   wordsToLower(arrayWords1, wordsNumber);
   
   /*
@@ -76,6 +75,7 @@ int main() {
   
   
   radixSort(arrayWords1, arrayWords2, arrayKey, largestWordLength, wordsNumber);
+  
   
   for (i = 0; i < wordsNumber; i++) {
     printf("%s\n", arrayWords2[i]);
@@ -234,234 +234,10 @@ int checkWordsValidity(char **arrayWords, int wordsNumber) {
 
 
 
-/*
-int radixSort(char**, char**, char*, int, int);
-int countingSort(char**, char**, int*, int);
-
-int checkWordsValidity(char**, int);
-int fillWords(char**, int, int);
-int wordsToLower(char**, int);
-int findLargestWordLength(char**, int);
-int* makePositionsArray(char**, char*, int*, int , int);
-
-
-
-
-//--- Radix and counting sort implementations
-
-  //The first input is the words' number, then the alphabetical key in a chosen order
-  //and all the words separated by a space.
-
-
-int main() {
-  int i, wordsNumber, keyLength = 27, largestWordLength;
-  char **arrayWords1, **arrayWords2, *arrayKey;
-  
-  scanf("%d", &wordsNumber);
-  
-  arrayKey = (char*) malloc(keyLength * sizeof(char));
-  arrayWords1 = (char**) malloc((wordsNumber + 1) * sizeof(char*));
-  arrayWords2 = (char**) malloc((wordsNumber + 1) * sizeof(char*));
-  
-  for (i = 0; i <= wordsNumber; i++) {
-    arrayWords1[i] = (char*) malloc(MAX_WORD_LENGTH * sizeof(char));
-    arrayWords2[i] = (char*) malloc(MAX_WORD_LENGTH * sizeof(char));
-  }
-  
-  
-  scanf("%s", arrayKey);
-  
-  // Rearranging the key with a blank space at the beginning.
-  for (i = keyLength - 1; i > 0; i--) {
-    arrayKey[i] = arrayKey[i - 1];
-  }
-  
-  arrayKey[0] = ' ';
-  
-  
-  // Give me all words.
-  for (i = 1; i <= wordsNumber; i++) {
-    scanf("%s", arrayWords1[i]);
-  }
-  
-  
-  wordsToLower(arrayWords1, wordsNumber);
-  
-  if (!checkWordsValidity(arrayWords1, wordsNumber)) {
-    return 0;
-  }
-  
-  for (i = 1; i <= wordsNumber; i++) {
-    printf("%s.\n", arrayWords1[i]);
-  }
-  
-  largestWordLength = findLargestWordLength(arrayWords1, wordsNumber);
-  
-  printf("%d\n", largestWordLength);
-  
-  fillWords(arrayWords1, wordsNumber, largestWordLength);
-  
-  
-  radixSort(arrayWords1, arrayWords2, arrayKey, largestWordLength, wordsNumber);
-  
-  for (i = 1; i <= wordsNumber; i++) {
-    printf("%s\n", arrayWords1[i]);
-  }
-  
-  
-  for (i = 0; i <= wordsNumber; i++) {
-    free(arrayWords1[i]);
-    free(arrayWords2[i]);
-  }
-  
-  free(arrayWords1);
-  free(arrayWords2);
-  free(arrayKey);
-  
-  return 0;
-}
-
-
-
-
-int radixSort(char **arrayWords1, char **arrayWords2, char *arrayKey, int largestWordLength, int wordsNumber) {
-  int i, j, *wordsPosition = (int*) malloc(wordsNumber * sizeof(int));
-  
-  for (i = largestWordLength - 1; i >= 0; i--) {
-    wordsPosition = makePositionsArray(arrayWords1, arrayKey, wordsPosition, wordsNumber, i);
-    
-    countingSort(arrayWords1, arrayWords2, wordsPosition, wordsNumber);
-  }
-  
-  free(wordsPosition);
-  
-  return 0;
-}
-
-
-int countingSort(char **arrayWords1, char **arrayWords2, int *wordsPosition, int wordsNumber) {
-  int i, *array3, keyLength = 27;
-  
-  array3 = (int*) malloc(keyLength * sizeof(int));
-  
-  
-  for (i = 0; i < keyLength; i++) {
-    array3[i] = 0;
-  }
-  
-  for (i = 0; i < wordsNumber; i++) {
-    array3[wordsPosition[i]] = array3[wordsPosition[i]] + 1;
-  }
-  
-  for (i = 1; i < keyLength; i++) {
-    array3[i] = array3[i] + array3[i - 1];
-  }
-  
-  for (i = 0; i < keyLength; i++) {
-    printf("%d", array3[i]);
-  }
-  
-  printf("\n");
-  
-  for (i = wordsNumber; i >= 1; i--) {
-    arrayWords2[array3[wordsPosition[i - 1]]] = arrayWords1[i];
-    array3[wordsPosition[i - 1]] = array3[wordsPosition[i - 1]] - 1;
-  }
-  
-  
-  for (i = 1; i <= wordsNumber; i++) {
-    arrayWords1[i] = arrayWords2[i];
-  }
-  
-  free(array3);
-  
-  return 0;
-}
-
-
-// It makes an array with the number of each letter position in the key.
-int* makePositionsArray(char **arrayWords1, char *arrayKey, int *wordsPosition, int wordsNumber, int matrixLayer) {
-  int i, j;
-  
-  for (i = 1; i <= wordsNumber; i++) {
-    for (j = 1; j < strlen(arrayKey); j++) {
-      if (arrayWords1[i][matrixLayer] == arrayKey[j]) {
-        wordsPosition[i - 1] = j;
-        break;
-      }
-    }
-  }
-  
-  return wordsPosition;
-}
-
-
-int findLargestWordLength(char **arrayWords, int wordsNumber) {
-  int i, largestWordLength;
-  
-  largestWordLength = strlen(arrayWords[1]);
-  
-  for (i = 1; i <= wordsNumber; i++) {
-    if (largestWordLength < strlen(arrayWords[i])) {
-      largestWordLength = strlen(arrayWords[i]);
-    }
-  }
-  
-  return largestWordLength;
-}
-
-
-// Fill all words with blank spaces until the same length of the largest one.
-int fillWords(char **arrayWords, int wordsNumber, int largestWordLength) {
-  int i, j;
-  
-  for (i = 1; i <= wordsNumber; i++) {
-    for (j = strlen(arrayWords[i]); j < largestWordLength; j++) {
-      arrayWords[i][j] = ' ';
-    }
-  }
-  
-  return 0;
-}
-
-
-int wordsToLower(char **arrayWords, int wordsNumber) {
-  int i, j;
-  
-  for (i = 1; i <= wordsNumber; i++) {
-    for (j = 0; j < strlen(arrayWords[i]); j++) {
-      arrayWords[i][j] = tolower(arrayWords[i][j]);
-    }
-  }
-  
-  return 0;
-}
-
-
-// Checking words validity based on: the word must be made with alphabetical letters, upper and lower cases, only
-int checkWordsValidity(char **arrayWords, int wordsNumber) {
-  int i, j;
-  
-  for (i = 1; i <= wordsNumber; i++) {
-    for (j = 0; j < strlen(arrayWords[i]); j++) {
-      
-      if (arrayWords[i][j] < 'a' || arrayWords[i][j] > 'z') {
-        printf("A palavra %s eh invalida", arrayWords[i]);
-        
-        return 0;
-      }
-    }
-  }
-  
-  return 1;
-}
-*/
-
-
-
 
 /*
-//--- Counting sort and Radix sort for integers.
+//--- Radix and Counting sorts for integers.
+
 int countingSort(int*, int*, int, int);
 int findLargest(int*, int);
 
